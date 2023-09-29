ceec6bc0bc61e4e4d4dc1da2141cc4f183f8df5c
Lucia Copes <lucicopes@gmail.com>
2023-09-29 19:24:47 -0300

    ValidacionesComprasYViajes_29/9/2023

--- EmpresaEnvío/EmpresaEnvÍoDto/ClasesDto/CompraDto.cs
+++ EmpresaEnvío/EmpresaEnvÍoDto/ClasesDto/CompraDto.cs
@@ -1,14 +1,23 @@
-﻿namespace EmpresaEnvÍoDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpresaEnvÍoDto
 {
     public class CompraDto
     {
+        [Required(ErrorMessage = "No se cargo un codigo de compra")]
         public int CodigoCompra { get; set; }
+        [Required(ErrorMessage = "No se cargo un codigo de producto")]
         public int CodigoProducto { get; set; }
+        [Required(ErrorMessage ="No se cargo el DNI del cliente")]
         public int DNICliente { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime FechaVenta { get; set; }
+        [Required(ErrorMessage = "No se cargo la cantidad de producto a comprar")]
         public int CantComprada { get; set; }
         public DateTime FechaEntregaSolicitada { get; set; }
+        [DataType(DataType.DateTime)]
         public EstadosCompra EstadoCompra { get; set; }
+        [Required(ErrorMessage = "No se cargo un monto total de la compra")]
         public int MontoTotal { get; set; }
     }
 }
--- EmpresaEnvío/EmpresaEnvÍoDto/ClasesDto/ViajeDto.cs
+++ EmpresaEnvío/EmpresaEnvÍoDto/ClasesDto/ViajeDto.cs
@@ -1,11 +1,17 @@
-﻿namespace EmpresaEnvÍoDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpresaEnvÍoDto
 {
     public class ViajeDto
     {
+        [Required(ErrorMessage ="No se cargo un codigo de viaje")]
         public int CodigoUnicoViaje { get; set; }
         public int Patente { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime FechaEntregasDesde { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTime FechaEntregasHasta { get; set; }
+        [Required(ErrorMessage = "No se cargo un porcentaje de carga")]
         public int PorcentajeOcupacionCarga { get; set; }
         List<CompraDto>? ListadoCompras { get; set; }
     }
