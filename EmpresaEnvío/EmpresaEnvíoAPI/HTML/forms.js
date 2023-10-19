be41414794e896990e32c1d628f9c20a313d6b2d
AgustinDuelli <agustinsduelli@gmail.com>
2023-10-19 12:26:51 -0300

    Forms_HTML_ActStock

--- EmpresaEnvío/EmpresaEnvíoAPI/HTML/forms.js
+++ EmpresaEnvío/EmpresaEnvíoAPI/HTML/forms.js
@@ -1,68 +1,51 @@
-document.getElementById('FormCrearReq').addEventListener('submit', function (event) {
+document.getElementById('formActCliente').addEventListener('submit', function (event) {
     event.preventDefault();
-    document.getElementById('formResponse').style.display = "none";
-    document.getElementById('tablaReqPost').innerHTML = "";
-
-    const titulo = document.getElementById('Titulo').value;
-    const descripcion = document.getElementById('Descripcion').value;
-
-    const prioridad = prioridadEnum[document.getElementById('Prioridad').value];
-    const fechaVencimiento = document.getElementById('FechaVencimiento').value;
-
-    const Requerimiento = {
-        id: 0,
-        titulo: titulo,
-        descripcion: descripcion,
-        prioridad: prioridad,
-        fechaCreacion: fechaVencimiento,
-        fechaVencimiento: fechaVencimiento,
-        estado: 0
-    };
-
-    fetch('http://localhost:5232/Requerimiento', {
-        method: 'POST',
+    const stockNuevo = document.getElementById('nuevoStock').value;
+    const codigoProducto = document.getElementById('codigoProducto').value;
+    let url = new URL(`http://localhost:5024/Producto/${codigoProducto}`);
+    fetch(url, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(Requerimiento)
+        body: JSON.stringify(stockNuevo)
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data);
             showData(data);
-            alert('Procesamiento correcto');
         })
         .catch(error => {
             console.log(error);
-            alert('Error al procesar', error);
+            showError(error);
         });
 });
 
 function showData(data) {
-    var fila = document.createElement("tr");
-    fila.innerHTML = `
-                    <td>${data.id}</td>
-                    <td>${data.titulo}</td>
-                    <td>${data.descripcion}</td>
-                    <td>${Object.keys(prioridadEnum)[data.prioridad - 1]}</td>
-                    <td>${Object.keys(estadoEnum)[data.estado - 1]}</td>
-                    <td>${data.fechaVencimiento.slice(0, 10)}</td>
-                    <td>${data.fechaCreacion.slice(0, 10)}</td>
+    var respuesta = document.getElementById('formResponse');
+    respuesta.innerHTML = `
+                    <p>Código = ${data.codProducto}</p>
+                    <p>Nombre = ${data.nombreProducto}</p>
+                    <p>Marca producto = ${data.marcaProducto}</p>
+                    <p>Alto caja = ${data.altoCaja}</p>
+                    <p>Ancho caja = ${data.anchoCaja}</p>
+                    <p>Profundidad caja = ${data.profundidadCaja}</p>
+                    <p>Precio unitario = ${data.precioUnitario}</p>
+                    <p>Stock mínimo = ${data.stockMínimo}</p>
+                    <p>Stock total = ${data.stockTotal}</p>
                 `;
-    document.getElementById('tablaReqPost').appendChild(fila);
-    document.getElementById('formResponse').style.display = "block";
 }
 
-var prioridadEnum = {
-    "Mínima": 1,
-    "Baja": 2,
-    "Media": 3,
-    "Alta": 4,
-    "Máxima": 5
-}
-
-var estadoEnum = {
-    "Pendiente": 1,
-    "En progreso": 2,
-    "Completado": 3
+function showError(error) {
+    var respuesta = document.getElementById('formResponse');
+    respuesta.innerHTML = `
+        <p> Error!</p>
+        <br>
+        <p>${error.message}</p>
+    `;
 }
