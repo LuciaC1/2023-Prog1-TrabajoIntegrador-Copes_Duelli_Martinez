082cd8091049d78c4e60ef26ac65f4d38e968cb3
AgustinDuelli <agustinsduelli@gmail.com>
2023-10-22 18:29:30 -0300

    FuncionalidadesWeb_Web

--- EmpresaEnvío/EmpresaEnvíoAPI/HTML/ActualizarStock.js
+++ EmpresaEnvío/EmpresaEnvíoAPI/HTML/ActualizarStock.js
@@ -1,8 +1,14 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.info("Loaded")
+});
+
 document.getElementById('formActCliente').addEventListener('submit', function (event) {
     event.preventDefault();
     const stockNuevo = document.getElementById('nuevoStock').value;
     const codigoProducto = document.getElementById('codigoProducto').value;
-    let url = new URL(`http://localhost:5024/Producto/${codigoProducto}`);
+    //const host = 'http://localhost:5024';
+    const host = 'https://v7m6wdlx-7174.brs.devtunnels.ms'
+    let url = new URL(`${host}/Producto/${codigoProducto}`);
     fetch(url, {
         method: 'PUT',
         headers: {
@@ -11,14 +17,23 @@ document.getElementById('formActCliente').addEventListener('submit', function (e
         body: JSON.stringify(stockNuevo)
     })
         .then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 400) {
+                return response.json().then(data => {
+                    const errorDetails = data.map(error => error.errorDetail);
+                    return errorDetails;
+                });
+            } else {
+                throw new Error(response.status);
             }
-            return response.json();
         })
         .then(data => {
-            console.log(data);
-            showData(data);
+            if (Array.isArray(data)) {
+                showErrorDetails(data);
+            } else {
+                showData(data);
+            }
         })
         .catch(error => {
             console.log(error);
@@ -27,8 +42,10 @@ document.getElementById('formActCliente').addEventListener('submit', function (e
 });
 
 function showData(data) {
-    var respuesta = document.getElementById('formResponse');
+    respuesta = document.getElementById('formResponse');
     respuesta.innerHTML = `
+                    <br>
+                    <h3>Producto actualizado</h3>
                     <p>Código = ${data.codProducto}</p>
                     <p>Nombre = ${data.nombreProducto}</p>
                     <p>Marca producto = ${data.marcaProducto}</p>
@@ -36,9 +53,15 @@ function showData(data) {
                     <p>Ancho caja = ${data.anchoCaja}</p>
                     <p>Profundidad caja = ${data.profundidadCaja}</p>
                     <p>Precio unitario = ${data.precioUnitario}</p>
-                    <p>Stock mínimo = ${data.stockMínimo}</p>
+                    <p>Stock mínimo = ${data.stockMinimo}</p>
                     <p>Stock total = ${data.stockTotal}</p>
                 `;
+}
+
+function showErrorDetails(data) {
+    const errores = data.map(error => `<br><p>${error}</p>`).join('');
+    const errorHTML = `<ul>${errores}</ul>`;
+    document.getElementById('formResponse').innerHTML = `<br><h3>Error!</h3>` + errorHTML;
 }
 
 function showError(error) {
